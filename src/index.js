1f3ccb31b283d1b6e03379d9c3550f5ba0207206
Advik <adviksingh6632@gmail.com>
2024-03-02 19:33:09 +0530

    feat: boom!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request) {
+		// if /pdf is requested, serve the pdf file.
+		// if (request.url.includes('/pdf')) {
+			let response = await fetch(`https://github.com/ADV1K/ADV1K/releases/latest/download/resume-advik.pdf`);
+			return new Response(response.body, {
+				headers: {
+					"content-type": "application/pdf",
+					"content-disposition": "inline; filename=Advik's Resume.pdf"
+				},
+			});
+		// }
+
+  },
 };
+
+// 2. Serve the pdf using drive.google.com pdf viewer. taken from vibhakar.
+// const html = `<!DOCTYPE html>
+// <style>
+//   body {
+//     height: 100dvh;
+//     overflow: hidden;
+//     margin:0;
+//   }
+//   .ndfHFb-c4YZDc-q77wGc {
+//     display: nonel
+//   }
+// </style>
+// <body>
+//   <iframe
+//     src="https://drive.google.com/file/d/1NkpwiLwOf_wiNVeSPaKFKsBzwj80DA3N/preview"
+//     allow="autoplay"
+//     height="100%"
+//     width="100%"
+//     style="border: none;">
+//     </iframe>
+// </body>`;
+//
+// return new Response(html, {
+//   headers: {
+//     "content-type": "text/html;charset=UTF-8",
+//   },
+// });
+
